bed7e5e28480f61807ccdffb097653419c83c911
Cameron Green <cameron@camerongreens.com>
2019-10-04 23:14:06 -0500

    Gimme a playground!

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const resolvers = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, playground: true })
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
